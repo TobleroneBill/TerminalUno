08dda7d03037504d80d3a86583e1abbb7c0cc739
TobeleroneBill <billseaton2.0@gmail.com>
2023-05-05 10:46:25 +0100

    More Setup

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # Uno - Bill Seaton - May 2023
 
 # Main features in the readme
+# Switching from curses to colorama, as it is a bit too advanced/ hopefully uneeded functionality
+
 # TODO: Make class structure for Player, Cards, Deck and Game Manager
 import json
 import random
-#import curses
+import curses   
 
     
 # init value changes the type of card (0-9 are normal, 10 - skip, 11 - Reverse, 12 - +2, 13 - +4 wild, 14 - wild)
@@ -42,6 +44,9 @@ class Deck:
     def printCards(self):
         for card in self.cards:
             card.printCard()
+    
+    def printDeckSize(self):
+        print(len(self.cards))
 
 
 # Has an AI setting so it can be used as a CPU Opponent
@@ -80,8 +85,6 @@ class GameManager:
 
 
 
-
-
 if __name__ == "__main__":
     with open('Settings.json','r') as settings:
         settingArgs = []
@@ -92,4 +95,6 @@ if __name__ == "__main__":
     #GM.Deck.printCards()
     GM.Deck.Shuffle()
     GM.Deck.printCards()
+    GM.Deck.printDeckSize()
+
 
